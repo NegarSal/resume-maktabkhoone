7d1cbba89840f0bf45ffcc6ace747c8f1d3cef2d
NegarSal <negarsaljooghi@gmail.com>
2020-02-12 01:58:45 +0330

    Updates

---
+++ precache-manifest.5411a2c356a43d090a565e2c1054dbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f4766c2ded68a20e780ab5b5f4adc30",
+    "url": "/resume-maktabkhoone/index.html"
+  },
+  {
+    "revision": "2e30f0fba8e3ab489108",
+    "url": "/resume-maktabkhoone/static/css/main.b6f4f19a.chunk.css"
+  },
+  {
+    "revision": "a65db098186cae051a68",
+    "url": "/resume-maktabkhoone/static/js/2.829eb2e1.chunk.js"
+  },
+  {
+    "revision": "7aa5a1bbf624454b84263d968b0b60cd",
+    "url": "/resume-maktabkhoone/static/js/2.829eb2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e30f0fba8e3ab489108",
+    "url": "/resume-maktabkhoone/static/js/main.b7184116.chunk.js"
+  },
+  {
+    "revision": "c1ddc3fb1d179661721b",
+    "url": "/resume-maktabkhoone/static/js/runtime-main.e485e9e1.js"
+  }
+]);
